4cd962df24f57b8ecc6d585c34883bb960755e0e
simpu <id.simpu@gmail.com>
2019-10-22 19:19:06 +0530

    added the function to read input asm file

--- register-renaming/main.cpp
+++ register-renaming/main.cpp
@@ -1,9 +1,32 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
+int main(int argc, char* argv[]) {
+	if (argc < 3) {
+		cout << "argument error";
+		return -1;
+	}
+	string inputFileName = string(argv[1]);
+	string outputFileName = string(argv[2]);
+	cout << "input file:" << inputFileName << endl;
+	cout << "output file:" << outputFileName << endl;
+	vector<string> inputCode;
+	ifstream file(inputFileName);
+	if (!file) {
+		cout << "error reading input file:" << inputFileName << endl;
+		return -1;
+	}
+	string line;
+	while (getline(file, line)) {
+		inputCode.push_back(line);
+	}
+	file.close();
+	for (size_t i = 0; i < inputCode.size(); i++) {
+		cout << inputCode[i] << endl;
+	}
 	return 0;
 }
